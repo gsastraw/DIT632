dc9e5c1e451a3ef3c515c9fc3d760fef42643f1c
juliavankirk <juliavankirk@gmail.com>
2022-02-10 02:33:48 +0100

    explanatory comments provided

--- wp3/exercise3.c
+++ wp3/exercise3.c
@@ -3,27 +3,46 @@
 
 #define TEMPERATURE_SENSOR A1
 
-int light_intensity;
-double temperature;
+int light_intensity; //the range of light intensity from 0-100
+double temperature;  //the value of temperature measured in celsius
 
-double convertTemperature(int sensorReading) {
-  double voltage = (sensorReading * 5.0) / 1024.0;
-  temperature = (voltage - 0.5) * 100;
+/**
+ * @brief this function serves as a mathematical computation to convert the sensor reading into 
+ *        a temperature. Firstly, there is a formula that converts the sensor reading into a voltage,
+ *        which is then used in a formula used to get a temperature reading.
+ * @param sensorReading 
+ * @return ** double 
+ */
+double convertTemperature(int sensorReading)
+{
+  double voltage = (sensorReading * 5.0) / 1023.0; //calculation for voltage per unit
+  temperature = (voltage - 0.5) * 100;             //calculation for temperature
   return temperature;
 }
 
+/**
+ * @brief This is the setup function. This is where the hardware is instantiated into the code.
+ * 
+ * @return ** void 
+ */
 void setup()
 {
-  pinMode(TEMPERATURE_SENSOR, INPUT);
+  pinMode(TEMPERATURE_SENSOR, INPUT); //these are the hardware used on the Arduino circuitry
   Serial.begin(9600);
 }
 
+/**
+ * @brief This is a loop function that increments every 2 seconds. For every 2 seconds, the temperature
+ *        is read and measured via the temperature sensor. The reading is then printed onto serial.
+ * 
+ * @return ** void 
+ */
 void loop()
 {
-  temperature = convertTemperature(analogRead(TEMPERATURE_SENSOR));
-  
-  Serial.print("TEMPERATURE LEVEL IS ");
-  Serial.println(temperature);
+  temperature = convertTemperature(analogRead(TEMPERATURE_SENSOR)); //the value of temperature is assigned from the input passed as a params to convertTemperature
+
+  Serial.print("TEMPERATURE LEVEL IS "); //String is printed onto serial to serve as information
+  Serial.println(temperature);           //value of temperature is printed onto serial
   delay(2000);
   // Serial.println(value);
 }
