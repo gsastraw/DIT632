371cd901be6e5c89d87a7ddaf89d87c7b8f06163
juliavankirk <juliavankirk@gmail.com>
2022-02-18 17:36:44 +0100

    changes

--- wp4/exercise3.ino
+++ wp4/exercise3.ino
@@ -1,98 +1,120 @@
 // (C) Gregory Sastrawidjaya, Klara Svensson, Julia Van Kirk, group: 31
 // Work package 3
 // Exercise 2
-// Submission code:
+// Submission code: ieBXfeFq
 
-#include <Arduino.h>           // Arduino library
-#include <Adafruit_NeoPixel.h> // NeoPixel library
+#include <Adafruit_NeoPixel.h> // header for the led ring functionality
 
-#define neo_pin = 10    // the pin number of the neopixel strip
-#define num_pixels = 16 // number of the leds
+#define NUM_PIN 10
+#define LED_COUNT 16
+#define DELAY 100
+#define RED_LED 5
 
-int delay_val = 100; // delay value
-int temperature;     // temperature
-int red = 15;        // rgb red value
-int green = 25;      // rgb green value
-int blue = 35;       // rgb blue value
+//instantiation of the neo pixel
+Adafruit_NeoPixel neo_pixel = Adafruit_NeoPixel(LED_COUNT, NUM_PIN, NEO_GRB + NEO_KHZ800);
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(num_pixels, neo_pin, NEO_GRB + NEO_KHZ800); // instantiation of neopixel
-
-float range[] = {-30.0, -20.0, -10.0, 0.0, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0, 110.0, 120.0};
-// array of breakpoints for the temperature
+int temp;
 
 /**
  * @brief this function serves as a mathematical computation to convert the sensor reading into 
  *        a temperature. Firstly, there is a formula that converts the sensor reading into a voltage,
- *        which is then used in a formula used to get a temperature reading.
- * 
  */
-void measure_temperature() // function that measures temperature
+void measure_temperature() // function to check the temperature
 {
-  int temperature = analogRead(A0);           // reads temperature from from analog pin
-  float voltage = temperature * (5.0 / 1024); // conversion analog to voltage
-  temperature = (voltage - 0.5) * 100;        // conversion from voltage get temperature
-}
-
-void setColors() // function to set the colors on the LED ring
-{
-  uint16_t i;              // unsigned 16 bit integer
-  for (i = 0; i < 16; i++) // for loop for range of LEDs on the ring
-  {
-    if (temperature > range[i]) // if the temperature is in the correct range
-    {
-      pixels.setPixelColor(i, getColor((i * 16) & 255)); // set the color to display on the LED ring
-      pixels.show();                                     // re-renders the leds on the ring
-      if (i == 15)
-      {
-        digitalWrite(3, HIGH);
-      }
-    }
-    else // else if its in the wrong range
-    {
-      pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // turns off the ring's lights (all rgb values 0)
-    }
-  }
-}
-
-uint32_t getColor(byte color) // function for getting rainbow colours, unsigned 32 bit integer
-{
-  /* function to get rainbow colours for the Neopixel ring by mattnupen on codebender. */
-  if (color < 85) // if the value is less than 85
-  {
-    return pixels.Color(color * 3, 255 - color * 3, 0); // increase the red, decrease the green, and set blue to 0
-  }
-  else if (color < 170) // if the value is between 85 and 170
-  {
-    color -= 85;
-    return pixels.Color(255 - color * 3, 0, color * 3); // decrease the red, set green to 0, and increase the blue
-  }
-  else // if the value is between 170 and 255
-  {
-    color -= 170;                                       // value is equal to the value minus 170
-    return pixels.Color(0, color * 3, 255 - color * 3); // set red to 0, increase the green, and decrease the blue
-  }
+  int temperature = analogRead(A0);           //reads temp from AO
+  float voltage = temperature * (5.0 / 1024); //calculation for voltage
+  temp = (voltage - 0.5) * 100;               //calculation for temperature
 }
 
 /**
- * @brief Sets up pin modes, neopixel begins, serial begins, and brightness is set
- * 
+ * @brief setup function that initializes pins and the hardwares connected to them
  */
 void setup()
 {
-  // put your setup code here, to run once:
-  pinMode(2, INPUT);         // set analogue pin 0 to input
-  pixels.begin();            //to start the led ring
-  Serial.begin(9600);        // initializes serial
-  pixels.setBrightness(120); // half brightness
+  pinMode(RED_LED, OUTPUT);
+  pinMode(A0, INPUT);
+  neo_pixel.begin();
+  Serial.begin(9600);
+  neo_pixel.setBrightness(120);
 }
 
 /**
- * @brief 
- * loop function that holds logic for the program
+ * @brief loop function that takes in a delay where the temperature is measured every 100ms and the colors
+ *        are set depending on the if the conditionals are fulfilled. The temperature is read and printed onto
+ *        serial for proper measurement. 
  */
 void loop()
 {
-  measure_temperature(); // calls function to measure temperature
-  setColors();           // calls function to set colors
-  delay(delay_val);      // delay
+  measure_temperature(); // call function to check the temperature
+  set_rgb();             // call function to set the colors
+  Serial.println(temp);
+  delay(DELAY); // delay (100 arbitrarily chosen)
+}
+
+//ranges for intervals
+int interval[] = {-30, -20, -10, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120};
+
+/**
+ * @brief Set the Colors object on the neopixel
+ * 
+ */
+void set_rgb() // function to set the colors on the LED ring
+{
+  uint16_t i; // unsigned int of 16 bits
+  //loop that iterates through every light
+  for (i = 0; i < 16; i++)
+  {
+    //if temperature surpasses range interval
+    if (temp > interval[i])
+    {
+      //a light on the neopixel will come alight for every interval surpassed in the predefined ranges,
+      neo_pixel.setPixelColor(i, get_rgb((i * 16) & 255)); //this is a function that is provided in the Adafruit_Neopixel library where we pass in count times number of lights
+      neo_pixel.show();                                    //another function within the Adafruit_Neopixel Library to show the light
+      //once the cursor has reached the 16th element, the standard red LED must come alight
+      if (i == 15)
+      {
+        //red led turns on
+        digitalWrite(RED_LED, HIGH);
+      }
+      else if (i < 16)
+      {
+        //red lef turns off
+        digitalWrite(RED_LED, LOW);
+      }
+      else
+      { //conditional statement to turn off the neo pixel if the conditionals are not fulfilled
+        neo_pixel.setPixelColor(i, neo_pixel.Color(0, 0, 0));
+      }
+    }
+  }
+}
+
+/**
+ * @brief Get the Color object; gets rainbow colors for the neo pixel
+ *        code taken from https://codebender.cc/sketch:80438#Neopixel%20Rainbow.ino
+ * 
+ * @param color 
+ * @return uint32_t 
+ */
+uint32_t get_rgb(byte color)
+{
+  // Input a value 0 to 255 to get a color value.
+  // The colours are a transition r - g - b - back to r.
+  if (color < 85) // if the value is less than 85
+  {
+    //red is upped, green comes down, blue is off
+    return neo_pixel.Color(color * 3, 255 - color * 3, 0);
+  }
+  else if (color < 170) // if less than 170
+  {
+    color -= 85; //85 deducted from sum total
+    //red is down, green is off, blue is upped
+    return neo_pixel.Color(255 - color * 3, 0, color * 3);
+  }
+  else //for all values above 170
+  {
+    color -= 170; //170 is deducted from the sum total
+    //red is off, green is upped, blue comes down
+    return neo_pixel.Color(0, color * 3, 255 - color * 3);
+  }
 }
